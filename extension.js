1fac6ab462b4ee7771aee07b259ab5035b8e700a
Jasur Sadikov <jasur@sadikoff.com>
2024-02-02 10:50:59 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -5,16 +5,32 @@ export default class LanguageFlags extends Extension {
 	constructor(metaData) {
 		super(metaData);
 		this.extensionDir = metaData.path;
-		this.evdevPath = `/usr/share/X11/xkb/rules/evdev.xml`;
+		this.evdevPath = '/usr/share/X11/xkb/rules/evdev.xml';
 		this.backupPath = `${this.extensionDir}/evdev.bkp`;
-		this.outputPath = `${this.extensionDir}/evdev/emoji.xml`;
+		this.glyphs2flagsPath = `${this.extensionDir}/glyphs2flags.py`;
+		this.outputPath = `${this.extensionDir}/evdev.xml`;
+		this.defaultResultPath = `${this.extensionDir}/evdev/emoji.xml`;
 	}
+	
+	_executeCommand(command) {
+		let [success, , , exitStatus] = GLib.spawn_command_line_sync(command);
+		return success && exitStatus === 0;
+	}
+
 	enable() {
-		if (!GLib.file_test(this.backupPath, GLib.FileTest.EXISTS))
-			GLib.spawn_command_line_sync(`cp "${this.evdevPath}" "${this.backupPath}"`);
-		GLib.spawn_command_line_sync(`cp "${this.outputPath}" "${this.evdevPath}"`);
+		if (!GLib.file_test(this.backupPath, GLib.FileTest.EXISTS)) {
+			this._executeCommand(`cp "${this.evdevPath}" "${this.backupPath}"`);
+		}
+		
+		const executedSuccessfully = this._executeCommand(`python3 "${this.glyphs2flagsPath}"`) &&
+		                             this._executeCommand(`cp "${this.outputPath}" "${this.evdevPath}"`);
+		
+		if (!executedSuccessfully) {
+			this._executeCommand(`cp "${this.defaultResultPath}" "${this.evdevPath}"`);
+		}
 	}
+
 	disable() {
-		GLib.spawn_command_line_sync(`cp "${this.backupPath}" "${this.evdevPath}"`);
+		this._executeCommand(`cp "${this.backupPath}" "${this.evdevPath}"`);
 	}
 }
